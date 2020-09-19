c8129a35cb4257f24454137d75f573d97aa24c9b
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-09-19 13:29:11 +0200

    v1

--- src/app/app.py
+++ src/app/app.py
@@ -1,2 +1,22 @@
+import random
+from os import system, name
+
+# define our clear function 
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+    else: 
+        _ = system('clear') 
+
+
 def app(flashcards):
-    pass
+    while True:
+        clear()
+        print("FlashCardsApp", "\n")
+        num = random.randint(0,len(flashcards)-1)
+        print("Polish:", "\n", flashcards[num].pw, "\n", flashcards[num].ps)
+        x = input()
+        print("English:", "\n", flashcards[num].ew, "\n", flashcards[num].es)
+        x = input()
+        if x == "end":
+            break
